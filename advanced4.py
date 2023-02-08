ccc839c7ab68d1a5531a27958e168a423242d456
JungWoo <100908884+JwLDevs@users.noreply.github.com>
2023-02-08 16:48:40 +0800

    edited a4

--- advanced4.py
+++ advanced4.py
@@ -5,7 +5,7 @@ import random
 def lucky_number(name, shoe_size, age):
     print("Your luckyy number is: " + str(round(len(name) - shoe_size + age)))
 
-randomName = ["Joe", "Thomas", "Don", "Howard", "Sammy", "Bobson", "Johny"]
+randomName = ["Joe", "Thomas", "Don", "Howard", "Sammy", "Bobson", "Johny", "Edward", "Jenny", "Lily", "Sally", "Samantha"]
 randomAge = random.randint(7, 80)
 shoeSize = random.randint(6, 12)
 
