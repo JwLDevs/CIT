aa5d9cdc197364a4d1974c27a6e1cb6b1a1f87df
JungWoo <100908884+JwLDevs@users.noreply.github.com>
2023-02-08 17:45:25 +0800

    a5 done

--- advanced5.py
+++ advanced5.py
@@ -1,4 +1,14 @@
 # Name generator
 
+import random
 
+name = input("Type [y] for a random full name my dei ")
 
+list_first_name = ["Ann", "Matt", "Daniel"]
+list_family_name_start = ["Green", "Wein", "Good"]
+list_family_name_end = ["smith", "son", "berg"]
+
+if (name == "y"):
+    print(str(random.choice(list_first_name) + " " + random.choice(list_family_name_start)) + random.choice(list_family_name_end))
+else:
+    print("ok didnt ask ratio + bozo L ")
