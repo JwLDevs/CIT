91d75a76b017ef909a1331f0a69805c249c84f2b
JungWoo <100908884+JwLDevs@users.noreply.github.com>
2023-02-08 18:04:47 +0800

    a3 done

--- advanced3.py
+++ advanced3.py
@@ -1,3 +1,10 @@
 # Rap name generator - Advanced 3
+import string
+import random
 
-#i dont fucking understand what the text book is saying
+first_name = input("What is your first name? ")
+random_first_letter = random.choice(string.ascii_letters)
+random_prefix = ["Lil", "Dj"]
+
+
+print("\nHmm.. how about " + '\033[1m' +  random.choice(random_prefix) + " " + first_name.replace(first_name[0], random_first_letter.upper()) + '\033[0;0m' + "?")
