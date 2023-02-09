27543c8b5d18b365d1835d54696351a24766994d
JungWoo <100908884+JwLDevs@users.noreply.github.com>
2023-02-09 11:38:55 +0800

    update

--- easy2.py
+++ easy2.py
@@ -1,8 +1,11 @@
 # Print words
+import time
+import random
 
 print_words = input("yo whats up ")
 
 random_word= ["hello", "hi", "hey", "yo", "sup"]
 
 for i in range(1000):
-    print(random_word[i % 5])
+    print(random.choice(random_word))
+    time.sleep(1)
